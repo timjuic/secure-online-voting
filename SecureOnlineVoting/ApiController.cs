55d1bde4e827405353a1c4d396e37c47f3064dc8
Danijel Žebčević <dzebcevic20@foi.hr>
2024-01-09 22:53:57 +0100

    napravljeno da se moze testirati sa swaggerom, dodana testna metoda

---
+++ SecureOnlineVoting/ApiController.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureOnlineVoting
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("smt")]
+        public ActionResult<string> ReturnsSomething()
+        {
+            return "String kul";
+        }
+    }
+}
--- SecureOnlineVoting/Program.cs
+++ SecureOnlineVoting/Program.cs
@@ -1,27 +1,44 @@
+using Microsoft.OpenApi.Models;
 using SecureOnlineVoting.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddControllers();
+
+
+var services = builder.Services;
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+//builder.Services.AddSwaggerGen();
+
+/*if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+}*/
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
 
-app.UseStaticFiles();
-app.UseAntiforgery();
+app.MapControllers();
+//app.UseStaticFiles();
+//app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+//app.MapRazorComponents<App>()
+// .AddInteractiveServerRenderMode();
 
 app.Run();
