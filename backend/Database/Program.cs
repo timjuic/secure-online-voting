1168995c2ac726f6de2cc2836c023c19a4b218c5
timjuic <tim.juic@gmail.com>
2024-01-11 18:25:57 +0100

    Add sqlite dependencies. run sqlite script

--- backend/Database/Program.cs
+++ backend/Database/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Data.SQLite;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string databaseFilePath = "database.sqlite";
+        string scriptFilePath = "db-script.sql";
+
+        string connectionString = $"Data Source={databaseFilePath};Version=3;";
+
+        try
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string script;
+                using (StreamReader reader = new StreamReader(scriptFilePath))
+                {
+                    script = reader.ReadToEnd();
+                }
+
+                using (SQLiteCommand command = new SQLiteCommand(script, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                Console.WriteLine("Database and tables created successfully.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
